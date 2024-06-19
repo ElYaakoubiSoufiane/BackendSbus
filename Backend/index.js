7d74b812ff3acad8dd81c75bcde4d50453f28bee
unknown <soufiane0elyaakoubi@gmail.com>
2024-06-19 23:59:10 +0100

    Initial Commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,33 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
-import userRouter from "./routers/Users"; // Assuming this is the correct path to your userRouter
-import authRoutes from "./routers/auth"; // Assuming this is the correct path to your userRouter
-import cardUser from "./routers/Cards"; // Assuming this is the correct path to your userRouter
-import { User } from "./models/User";
-import  Employees from "./routers/employees"
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const Users_1 = __importDefault(require("./routers/Users")); // Assuming this is the correct path to your userRouter
+const auth_1 = __importDefault(require("./routers/auth")); // Assuming this is the correct path to your userRouter
+const Cards_1 = __importDefault(require("./routers/Cards")); // Assuming this is the correct path to your userRouter
+const employees_1 = __importDefault(require("./routers/employees"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 // Connect to MongoDB
-mongoose
-  .connect(process.env.DB_URL!, {})
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
-
+mongoose_1.default
+    .connect(process.env.DB_URL, {})
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
 // Use the userRouter for handling user-related routes
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRoutes);
-app.use("/card", cardUser);
-app.use("/employees", Employees);
+app.use("/api/users", Users_1.default);
+app.use("/api/auth", auth_1.default);
+app.use("/card", Cards_1.default);
+app.use("/employees", employees_1.default);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
 // app.use(asyncHandler);
-
 // app.post("/createPaymentIntent", async (req, res) => {
 //   const customer = await stripe.customers.create();
 //   const ephemeralKey = await stripe.ephemeralKeys.create(
@@ -44,7 +43,6 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 //       enabled: true,
 //     },
 //   });
-
 //   res.json({
 //     paymentIntent: paymentIntent.client_secret,
 //     ephemeralKey: ephemeralKey.secret,
@@ -53,25 +51,19 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 //       "pk_test_51Ob3OODYnDpmKN8R8iAetLdazqre5NAGycmNADOTVCFROkUY7oqGQ1gdLrP5h4lEMfytOsnxq2uQXnubpJdmFKQA005diGufQc",
 //   });
 // });
-
 // Adjust the path as necessary
-
 // async function createCardForUser(userId, cardData) {
 //   // Find the user by ID
 //   const user = await User.findById(userId);
 //   if (!user) throw new Error('User not found');
-
 //   // Create a new card and link it to the user
 //   const card = new Card({ ...cardData, user: user._id });
 //   await card.save();
-
 //   // Add the card to the user's cards array
 //   user.cards.push(card._id);
 //   await user.save();
-
 //   return card;
 // }
-
 // app.post("/getTicket", async (req, res) => {
 //   res.send(req.body);
 //   console.log("-------------", req.body);
@@ -92,10 +84,8 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 //     success_url: "https://example.com/success",
 //     cancel_url: "https://example.com/cancel",
 //   });
-
 //   console.log(session);
 // });
-
 // const stripe = require("stripe")(
 //   "sk_test_51Ob3OODYnDpmKN8R1k7V6163qlEHFmQaVRTXXgNdTJ0oN89kQByj8cij95mG9p44G8Pj4wlYFXVjciqKHXAm1Zoo00qa4uL0GD"
 // );
